c4f3490d74f739e62f30c7cd83516880bd898580
Robert <r.mill@ntlworld.com>
2016-08-25 01:57:44 +0100

    add css

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,10 +12,32 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/animate.min.css',
+        'css/bootstrap.min.css',
+        'css/font-awesome.css',
+        'css/font-awesome.min.css',
+        'css/main.css',
+        'css/prettyPhoto.css',
+        'css/responsive.css',
+
+
         'css/site.css',
     ];
     public $js = [
+        'assets/js/bootstrap.min.js',
+        'assets/js/html5shiv.js',
+        'assets/js/jquery.isotope.min.js',
+        'assets/js/jquery.js',
+        'assets/js/jquery.prettyPhoto.js',
+        'assets/js/main.js',
+        'assets/js/respond.min.js',
+        'assets/js/wow.min.js',
+
+
     ];
+
+
+
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
